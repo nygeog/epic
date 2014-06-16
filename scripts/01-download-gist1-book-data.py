61d4e62a7ee6037c572e90b399433d13c958faf3
nygeog <daniel.martin.sheehan@gmail.com>
2014-06-16 09:06:49 -0400

    Update 01-download-gist1-book-data.py

--- scripts/01-download-gist1-book-data.py
+++ scripts/01-download-gist1-book-data.py
@@ -3,6 +3,7 @@ import re, urllib, time, zipfile, os
 stop_time = 3
 
 download_location = 'E:/'
+#THIS MIGHT NEED to be changed to F: or some other drive letter
 #EXAMPLE for Mac download_location = '/Volumes/Echo/GIS/data/census/2000/blocks/'
 
 url = 'https://dl.dropboxusercontent.com/u/36281098/epic/data/gist1_instructor_edition.zip'
